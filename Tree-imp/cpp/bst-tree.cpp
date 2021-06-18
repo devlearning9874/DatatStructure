fb672055d89e851f79f9874e5af582409ed7a33e
devlearning9874 <devlearning9874@gmail.com>
2021-06-18 18:34:12 +0530

    Removed unncessary code

--- Tree-imp/cpp/bst-tree.cpp
+++ Tree-imp/cpp/bst-tree.cpp
@@ -154,36 +154,7 @@ void levelOrder(Node* root){
 
 }
 
-bool isSubTreeLesser(Node* root, int value){
 
-    if(root == NULL){
-          return true;
-    }
-
-    if(root->data <= value
-       && isSubTreeLesser(root->left, value)
-       && isSubTreeLesser(root->right, value)){
-           return true;
-       }else{
-       return false;
-    }
-       
-}
-
-bool isSubTreeGreater(Node* root, int value){
-    if(root==NULL){
-        return true;
-    } 
-
-    if(root->data > value
-       && isSubTreeGreater(root->left, value)
-       && isSubTreeGreater(root->right, value)){
-            return true;
-       }else{
-         return false;
-    }
-       
-}
 
 bool isItBSTool(Node* root, int minValue, int maxValue){
 
