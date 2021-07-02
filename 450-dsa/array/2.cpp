8355e67b6200da8adc142e5b36a01dd25cd61993
devlearning9874 <devlearning9874@gmail.com>
2021-07-02 18:53:19 +0530

    min max in array

---
+++ 450-dsa/array/2.cpp
@@ -0,0 +1,53 @@
+// Maximum and minimum of an array using minimum number of comparisons
+// https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class minMax{
+    public:
+       int min = 0;
+       int max = 0;
+
+      minMax(int arr[], int n){
+            
+            if(n==1){
+              min = max = arr[0];
+            } 
+                if(arr[0]> arr[1]){
+                    max = arr[0];
+                    min = arr[1];
+                }else{
+                    max = arr[1];
+                    min = arr[0];
+                }
+            
+            for(int i=2; i<n; i++){
+
+                if(arr[i]>max){
+                    max = arr[i];
+
+                }else if(arr[i] < min){
+                    min = arr[i];
+                }
+            }
+
+      }
+
+};
+
+
+ int main(){
+ 
+  int arr[] = {1,2,3,4,5,6};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  minMax obj(arr,n);
+  
+ cout<<"Maximum:"<<obj.max<<endl;
+ cout<<"Minimum:"<<obj.min<<endl;
+
+ return 0;
+
+}
