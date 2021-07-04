505f3fee8b85d0309a8dd585a0005b7a9619d23f
devlearning9874 <devlearning9874@gmail.com>
2021-07-04 22:44:12 +0530

    Implemented Selection Sort

--- sorting-algo/selection-sort/cpp/selection-sort.cpp
+++ sorting-algo/selection-sort/cpp/selection-sort.cpp
@@ -1,14 +1,44 @@
-// 
+// Running time analysis for Selection Sort
+// Space Complexity: O(1)
+// Time Complexity: O(n^2)
 
 #include<iostream>
-
+#include<algorithm>
 using namespace std;
 
+void swapA(int *a, int *b){
 
+    int temp = *a;
+     *a = *b;
+     *b = temp;
+
+}
+
+void selectionSort(int arr[], int n){
+      
+    
+    for(int i=0; i<n-1; i++){
+         
+         int min_index = i;
+         for(int j=i+1; j<n; j++){
+
+             if(arr[j]<arr[min_index]){
+                 min_index = j;
+             }
+         }
+         swapA(&arr[min_index], &arr[i]);
+    }
+}
 
 int main(){
+   int arr[] = {64, 25, 12, 22,11,9,9,7,3};
    
-   cout<<"Initializing selection sort";
-   
+   int n = sizeof(arr)/sizeof(arr[0]);
+
+   selectionSort(arr,n);
+   cout<<"Selection Sort:";
+   for(auto it: arr){
+       cout<<it<<" ";
+   }
    return 0;
 }
