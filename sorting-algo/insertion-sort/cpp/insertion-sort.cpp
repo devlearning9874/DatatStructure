aacdc770eccbe4e4459867ab9009b3d9ed08eb6d
devlearning9874 <devlearning9874@gmail.com>
2021-07-04 21:31:43 +0530

    Implemented insertion sort

--- sorting-algo/insertion-sort/cpp/insertion-sort.cpp
+++ sorting-algo/insertion-sort/cpp/insertion-sort.cpp
@@ -5,9 +5,36 @@
 using namespace std;
 
 
+void insertionSort(int arr[], int n){
+     
+     int i, j, key;
+     for(int i=1; i<n; i++){
+
+         int key = arr[i];
+         int j = i - 1;
+        while(j>=0 && key < arr[j]){
+
+            arr[j+1] = arr[j];
+            j--;
+        }
+
+        arr[j+1] = key;
+     }
+}
+
 int  main(){
     
-    cout<<"Initializing Insertion Sort!";
+   int arr[] = {4, 3, 2, 10, 12, 1, 5, 6};
+   
+   int n = sizeof(arr)/sizeof(arr[0]);
+
+   insertionSort(arr,n);
+
+   cout<<"Insetion Sort:";
+
+   for(auto it: arr){
+       cout<<it<<" ";
+   }
 
     return 0;
 }
