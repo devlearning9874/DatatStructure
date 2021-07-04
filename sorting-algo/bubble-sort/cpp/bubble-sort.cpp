01878abaff1b1c3eaa4f8726c70f064177c64488
devlearning9874 <devlearning9874@gmail.com>
2021-07-04 23:06:42 +0530

    Implemented Bubble Sort

--- sorting-algo/bubble-sort/cpp/bubble-sort.cpp
+++ sorting-algo/bubble-sort/cpp/bubble-sort.cpp
@@ -1,14 +1,45 @@
 //Running time analysis for Bubble Sort
 // Space Complexity: O(1)
 // Time Complexity: O(n^2)
-
+// This is easiest sorting algorithm one can relate it with brute-force approach
+// Sorting type is in-place because it takes constant memory
+// To optimize it further check bubble-sort-optimised.cpp
 #include<iostream>
 
 using namespace std;
 
+
+void bubbleSort(int arr[], int n){
+
+      int i,j;
+
+      for(i=0; i<n; i++){
+
+          for(j = i+1; j<n-1; j++){
+
+                if(arr[i] > arr[j]){
+                    swap(arr[i], arr[j]);
+                }
+
+          }
+      }
+
+}
+
+
 int main(){
     
-    cout<<"Initializing Bubble Sort";
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    bubbleSort(arr,n);
+    
+     cout<<"Bubble Sort:";
+
+    for(auto it: arr){
+        cout<<it<<" ";
+    }
 
     return 0;
 
