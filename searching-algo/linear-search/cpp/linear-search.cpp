90f0966a9e5450a7392c2ce53d5883234f0e6789
devlearning9874 <devlearning9874@gmail.com>
2021-07-05 22:19:29 +0530

    Implemented Linear Search

--- searching-algo/linear-search/cpp/linear-search.cpp
+++ searching-algo/linear-search/cpp/linear-search.cpp
@@ -4,9 +4,32 @@
 
 using namespace std;
 
+int linearSearch(int arr[], int n){
+
+    int key;
+    cout<<"Enter key to find:"<<endl;
+    cin>>key;
+
+    for(int i=0; i<n; i++){
+
+        if(key == arr[i]){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
 int main(){
+
+    int arr[] = {5,1,3,7,9,6,4,2};
     
-    cout<<"Linear Search Implementation";
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int pos = linearSearch(arr,n);
+
+    cout<<"Element found at position:"<<pos;
 
     return  0;
 }
