6fbf7a5129eb8fd472612912709f7f4a1f3441cb
devlearning9874 <devlearning9874@gmail.com>
2021-07-05 22:08:43 +0530

    Initializing Linear Search

---
+++ searching-algo/linear-search/cpp/linear-search.cpp
@@ -0,0 +1,13 @@
+//Time comlexity will O(n) in worst case, O(1) in best case
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout<<"Linear Search Implementation";
+
+    return  0;
+}
+
