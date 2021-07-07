e55edf90e0cf33a458b6c626fd71cac878826198
devlearning9874 <devlearning9874@gmail.com>
2021-07-07 21:00:38 +0530

    0-1knapsack solution using recursion

--- 450-dsa/dynamic-programming/0-1knapsack/cpp/recKnapsack.cpp
+++ 450-dsa/dynamic-programming/0-1knapsack/cpp/recKnapsack.cpp
@@ -1,10 +1,32 @@
 
 
 #include<iostream>
-
+#include<algorithm>
 using namespace std;
 
+int knapsack(int W, int values[], int weight[], int n){
+    int maxProfit;
+    if(n == 0 || W == 0) return 0;  //Base Case
+
+    if(weight[n] > W){
+        knapsack(W, values,weight,n-1);
+    }
+    else{
+        return  max(values[n]+knapsack(W-weight[n],values,weight,n-1),knapsack(W,values,weight, n-1));
+    }
+    
+   return 0;
+}
+
+
 int main(){
-    cout<<"knapsack Solution";
+   int n = 3;
+   int W = 4;
+   int values[] = {1,2,3};
+   int weight[] = {4,5,1};
+
+   cout<<"Maxm Profit:"<<knapsack(W, values, weight, n-1);
+    
+    
     return 0;
 }
