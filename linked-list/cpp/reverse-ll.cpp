0c8ade507d486d78540c60ed1fafb169f9554047
devlearning9874 <devlearning9874@gmail.com>
2021-07-11 01:44:30 +0530

    Reverse Linked List Iteratively

--- linked-list/cpp/reverse-ll.cpp
+++ linked-list/cpp/reverse-ll.cpp
@@ -4,10 +4,101 @@
 
 using namespace std;
 
+class Node{
+    public:
+      int val;
+      Node *next;   
+};
 
+class LinkedList{
+    public:
+      Node *head = NULL;
+      
+      void insertTail(int data){
+
+          Node *newNode = new Node();
+          newNode->val = data;
+          newNode->next = NULL;
+
+          if(head == NULL){
+              head = newNode;
+              return;
+          }
+
+          Node *temp = head;
+
+          while(temp->next != NULL){
+              temp = temp->next;
+          }
+
+          temp->next = newNode;
+      }
+      
+      Node* reverseLL(){
+
+          Node *next;
+          Node* current = head;
+          Node *prev = NULL;
+
+          while(current != NULL){
+             next = current->next;
+              current->next = prev;
+              prev = current;
+              current = next;
+          }
+
+          return prev;
+        
+      }
+
+      void print(){
+
+          if(head == NULL){
+              cout<<"List is Empty!"<<endl;
+              return;
+          }
+
+          Node *temp = head;
+
+          while(temp != NULL){
+              int value = temp->val;
+              cout<<value<<" ";
+              temp = temp->next;
+          }
+
+          cout<<endl;
+      }
+
+      void printReverse(Node* rev){
+
+          if(rev == NULL){
+              cout<<"Empty List!"<<endl;
+              return;
+          }
+
+          Node *temp = rev;
+
+          while(temp != NULL){
+              cout<<temp->val<<" ";
+              temp = temp->next;
+          }
+
+          cout<<endl;
+      }
+};
 int main(){
     
-    cout<<"Initialized revese ll";
+    LinkedList ll;
+    
+    ll.print();  // List is empty
+    ll.insertTail(5);
+    ll.insertTail(10);
+    ll.insertTail(15);
+    ll.insertTail(20);
+    ll.insertTail(25);
+    ll.print();
+    Node *rev = ll.reverseLL();
+    ll.printReverse(rev);
 
     return 0;
 }
