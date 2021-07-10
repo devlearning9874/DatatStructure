867897d1fef4ae1da31c298db04dd0642a7c975a
devlearning9874 <devlearning9874@gmail.com>
2021-07-11 00:33:49 +0530

    Deletenode at any position

--- linked-list/cpp/delete-in-ll.cpp
+++ linked-list/cpp/delete-in-ll.cpp
@@ -1,96 +1,146 @@
 
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-
-class Node{
-    public:
-       int val;
-       Node* next;
+class Node
+{
+public:
+    int val;
+    Node *next;
 };
 
-class LinkedList{
-     
-     public:
-       Node* head = NULL;
+class LinkedList
+{
 
-     void insertNodeFront(int data){
-           Node *newNode = new Node();
-           if(head == NULL){
-             newNode->val = data;
-             newNode->next = NULL;
-             head = newNode;
-             return;
-           }
-           newNode->val  = data;
-           newNode->next = head;
-           head = newNode;
-     }
+public:
+    Node *head = NULL;
 
-     void insertNodeTail(int data){
+    void insertNodeFront(int data)
+    {
+        Node *newNode = new Node();
+        if (head == NULL)
+        {
+            newNode->val = data;
+            newNode->next = NULL;
+            head = newNode;
+            return;
+        }
+        newNode->val = data;
+        newNode->next = head;
+        head = newNode;
+    }
 
-     }
+    void insertNodeTail(int data)
+    {
+    }
 
-     void insertNodePos(int data){
+    void insertNodePos(int data)
+    {
+    }
 
-     }
+    void deleteNodeFront()
+    {
 
-     void deleteNodeFront(){
+        if (head == NULL)
+        {
+            cout << "List is Empty!";
+            return;
+        }
 
-          if(head == NULL){
-              cout<<"List is Empty!";
-              return;
-          }
+        Node *temp = head;
 
-          Node *temp = head;
+        head = temp->next;
 
-          head = temp->next;
+        free(temp);
+    }
 
-          free(temp);
-     }
+    void deleteNodeTail()
+    {
 
-     void deleteNodeTail(){
+        if (head == NULL)
+        {
+            cout << "List is empty!";
+            return;
+        }
 
-         if(head == NULL){
-             cout<<"List is empty!";
-             return;
-         }
+        Node *temp = head;
 
-         Node *temp = head;
+        while (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        Node *temp1 = temp->next;
+        temp->next = NULL;
 
-         while(temp->next->next != NULL){
-             temp = temp->next;
-         }
-         Node *temp1 = temp->next;
-         temp->next = NULL;
+        free(temp1);
+    }
 
-         free(temp1);
+    void deleteNodePos(int pos){
+
+        if(head == NULL){
+            cout<<"List is Empty!";
+            return;
+        }
+
+        if(pos < 0){
+            cout<<"Invalid position";
+            return;
+        }
+
+        Node* temp = head;
+        pos = pos - 2;
+        while(pos > 0){
+            temp = temp->next;
+            pos--;
+        }
+
+        if(temp == NULL){
+            cout<<"Out of boundary!";
+            return;
+        }
+
+        if(temp->next == NULL){
+            deleteNodeTail();
+            return;
+        }
+
+        // cout<<temp->val<<"pos elemet";
+
+       Node* temp1 = temp->next->next;
+
+       free(temp->next);
+
+       temp->next = temp1;
 
 
-     }
+    }
 
-     void print(){
+    void print()
+    {
 
-         if(head == NULL){
-             cout<<"LL is Empty!"<<endl;
-             return;
-         }
+        if (head == NULL)
+        {
+            cout << "LL is Empty!" << endl;
+            return;
+        }
 
-         Node *temp = head;
-          
-          while(temp != NULL){
-              int value = temp->val;
-              cout<<value<<" ";
-              temp = temp->next;
-          }
-          cout<<endl;
-     }
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            int value = temp->val;
+            cout << value << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 
-int main(){
-    
+int main()
+{
+
     LinkedList ll;
 
     ll.insertNodeFront(5);
@@ -101,8 +151,10 @@ int main(){
     ll.insertNodeFront(30);
     ll.print();
     // ll.deleteNodeFront();
-    ll.deleteNodeTail();
+    // ll.deleteNodeTail();
+    // ll.print();
+    ll.deleteNodePos(3);
     ll.print();
 
-    return  0;
+    return 0;
 }
