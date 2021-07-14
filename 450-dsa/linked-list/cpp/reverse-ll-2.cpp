fb4e8261aafb21f421aca845e743d51e46b34ec2
devlearning9874 <devlearning9874@gmail.com>
2021-07-14 23:36:19 +0530

    Reverse Linked list init

---
+++ 450-dsa/linked-list/cpp/reverse-ll-2.cpp
@@ -0,0 +1,12 @@
+// https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Solution!";
+
+    return 0;
+}
